e9723e24477283a054b8fcbec210177f472b7d0b
ksw <zenuine1@gmail.com>
2018-10-10 18:35:15 +0900

    장난감 도서관 모델 생성

--- app/toy_library/models.py
+++ app/toy_library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Toylibrary(models.Model):
+    name = models.CharField(max_length=255)
+    gu = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address_road = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=25, decimal_places=25)
+    longitude = models.DecimalField(max_digits=25, decimal_places=25)
