305fe496b7b2e0afc57d1e47cfd9e753911bcd31
ksw <zenuine1@gmail.com>
2018-10-11 17:24:13 +0900

    장난감 도서관 리스트 출력 구현

--- app/toylibrary/models.py
+++ app/toylibrary/models.py
@@ -6,5 +6,8 @@ class Toylibrary(models.Model):
     gu = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     address_road = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=25, decimal_places=25)
-    longitude = models.DecimalField(max_digits=25, decimal_places=25)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
