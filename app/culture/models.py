1aa617c9132fe28391c6e7ca4714dba9f64fe4b3
ksw <zenuine1@gmail.com>
2018-10-31 16:50:35 +0900

    문화행사 모델링 구현

--- app/culture/models.py
+++ app/culture/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Culture(models.Model):
+    gu = models.CharField(max_length=255, blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    start_date = models.CharField(max_length=255, blank=True)
+    end_date = models.CharField(max_length=255, blank=True)
+    time = models.CharField(max_length=255, blank=True)
+    homepage = models.URLField()
+    target_user = models.CharField(max_length=255, blank=True, null=True)
+    fee = models.CharField(max_length=255, blank=True, null=True)
+    inquiry = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.place
