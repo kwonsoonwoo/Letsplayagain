c20d509fcee70c0a8ac6c65c85035b8710370194
ksw <zenuine1@gmail.com>
2018-11-05 18:36:36 +0900

    공원 모델링 및 어드민 등록

--- app/park/models.py
+++ app/park/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Park(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    overview = models.CharField(max_length=255, blank=True, null=True)
+    use_note = models.CharField(max_length=255, blank=True, null=True)
+    region = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    tell = models.CharField(max_length=255, blank=True, null=True)
+    latitude = models.CharField(max_length=255, blank=True, null=True)
+    longitude = models.CharField(max_length=255, blank=True, null=True)
+    website = models.CharField(max_length=255, blank=True, null=True)
